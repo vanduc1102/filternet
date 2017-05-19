a30b86980926ee2c1b5ebe2cb5bb4254094bd021
Duc NGUYEN <vanducld@gmail.com>
2017-05-19 18:24:30 +0700

    Add add show ip addresses and port

--- example/skeletontest.js
+++ example/skeletontest.js
@@ -1,10 +1,12 @@
 var proxy = require('../lib/proxy.js');
+var os = require('os');
+var ifaces = os.networkInterfaces();
 
+var PROXY_PORT = 8888;
+var myProxy = proxy.createProxyServer({ port: PROXY_PORT });
 
-var myProxy = proxy.createProxyServer({
-                                        port: 8088                                    });
-
-
+printAddresses();
+console.log('port : '+ PROXY_PORT);
 
 // Whether or not to enable custom intercepting at all.
 myProxy.on('shouldEnableInterception', function (callback) {
@@ -23,7 +25,7 @@ myProxy.on('shouldInterceptResponseContent', function (proxyResponse, callback) 
 // (just headers)
 myProxy.on('interceptRequest', function (requestInfo, callback) {
    // requestInfo is the same as the arguments to http.request
-   console.log(requestInfo.host, requestInfo.path);
+   console.log( requestInfo.host, requestInfo.path);
    callback(requestInfo);
 });
 
@@ -44,3 +46,26 @@ myProxy.on('error', function (error, locationInfo) {
    console.log(locationInfo);
    console.log(error.stack);
 });
+
+
+function printAddresses(){
+    Object.keys(ifaces).forEach(function (ifname) {
+        var alias = 0;
+
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false ) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+
+            if (alias >= 1) {
+                // this single interface has multiple ipv4 addresses
+                console.log(ifname + ':' + alias, iface.address);
+            } else {
+                // this interface has only one ipv4 adress
+                console.log(ifname + ':' + iface.address);
+            }
+            ++alias;
+        });
+    });
+}
